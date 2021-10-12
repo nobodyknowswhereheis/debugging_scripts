19df5c567ab1d69e46c68449c2badb6d54754d3b
Jesse Adams <nobodyknowswhereheis@yahoo.com>
2021-10-12 11:21:14 -0400

    better comments

--- sp-fieldWatcher.js
+++ sp-fieldWatcher.js
@@ -1,5 +1,7 @@
-var field = "nameOfSomeField";    // property to watch
-var prop = "value"; // what about the field do you want to watch?
+// get a stack trace when a specific proprty changes on a field in service portal.
+
+var field = "nameOfSomeField";    // field to watch eq. short_description
+var prop = "value"; // what about the field do you want to watch? eg. value, mandatory, read-only, visible
 
 var _field = $('sp-variable-layout').scope().formModel._fields[field];
 var wrapObject = function (obj, prop){
